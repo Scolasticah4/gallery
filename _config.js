d314e02340a87be6d15ff255b73282eebdd0fbc3
Scolasticah4 <wanjiruscola12@gmail.com>
2024-06-29 06:19:49 -0400

    Updated uRL

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://Scola:<DjDzNXawIbv92EJm>@devops.ouaw0jj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps',
-    development: 'mongodb+srv://Scola:<DjDzNXawIbv92EJm>@devops.ouaw0jj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps',
-    test: 'mongodb+srv://Scola:<DjDzNXawIbv92EJm>@devops.ouaw0jj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps',
-}
+    production: 'mongodb+srv://Scola:DjDzNXawIbv92EJm@devops.ouaw0jj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps',
+    development: 'mongodb+srv://Scola:DjDzNXawIbv92EJm@devops.ouaw0jj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps',
+    test: 'mongodb+srv://Scola:DjDzNXawIbv92EJm@devops.ouaw0jj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps',
+};
 module.exports = config;
