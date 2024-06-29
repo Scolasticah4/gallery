3ed5c76fcba4eb01f1a9537b39e3b276cea03637
Scolasticah4 <wanjiruscola12@gmail.com>
2024-06-29 05:24:50 -0400

    Update

--- _config.js
+++ _config.js
@@ -3,7 +3,7 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://Scola:<DjDzNXawIbv92EJm>@devops.ouaw0jj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps',
-    development: 'mongodb+srv://Scola:<DjDzNXawIbv92EJm>@devops.ouaw0jj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps',
-    test: 'mongodb+srv://Scola:<DjDzNXawIbv92EJm>@devops.ouaw0jj.mongodb.net/?retryWrites=true&w=majority&appName=DevOps',
-}
+    development: 'mongodb://localhost:27017/darkroom',
+    test: 'mongodb://localhost:27017/test',
+};s
 module.exports = config;
