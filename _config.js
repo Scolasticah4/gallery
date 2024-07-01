27af3aeae6b6b1e2396430b474f5dd20e7c8dfb2
Scolasticah4 <wanjiruscola12@gmail.com>
2024-07-01 06:27:43 -0400

    Update mongodb credentials

--- _config.js
+++ _config.js
@@ -2,8 +2,12 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+    production: 'mongodb+srv://Admin:admin@cluster0.o6auc1a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+,
+    development: 'mongodb+srv://Admin:admin@cluster0.o6auc1a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+,
+    test: 'mongodb+srv://Admin:admin@cluster0.o6auc1a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+,
+};
 module.exports = config;
